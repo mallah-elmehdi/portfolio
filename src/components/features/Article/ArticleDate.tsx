4ad459fb7d9dcf8ae7ff0a909da7956eab0bbaea
mallah-elmehdi <elmehdimallah@gmail.com>
2026-01-31 11:40:37 +0800

    fixing the date formatting warning: Use `yyy` instead of `YYY`

--- src/components/features/Article/ArticleDate.tsx
+++ src/components/features/Article/ArticleDate.tsx
@@ -2,5 +2,5 @@ import { ArticleType } from '@/types/article.type';
 import { format } from 'date-fns';
 
 export default function ArticleDate({ date }: Pick<ArticleType, 'date'>) {
-    return <p className="font-extralight tracking-wider text-gray-500 text-sm">{format(date, 'LL MMM YYY')}</p>;
+    return <p className="font-extralight tracking-wider text-gray-500 text-sm">{format(date, 'LL MMM yyy')}</p>;
 }
